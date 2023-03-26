0051acde8f7e2bf7a40de06a5daee1de026bcec7
SimonNyvall <simon.k.nyvall@gmail.com>
2023-03-26 19:37:46 +0200

    FizzBuzz

---
+++ FizzBuzz/Program.fs
@@ -0,0 +1,9 @@
+﻿let fizzbuzz maxNum =
+    for i = 1 to maxNum do
+        let fizz = if i % 3 = 0 then "Fizz" else ""
+        let buzz = if i % 5 = 0 then "Buzz" else ""
+        let number = if fizz = "" && buzz = "" then i.ToString() else ""
+        printfn $"%s{fizz}%s{buzz}%s{number}"              
+        
+fizzbuzz 100
+
